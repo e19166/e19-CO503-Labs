ad00b9f92e9a54145d33f6d0db6eb00c6132c451
e19166 <e19166@eng.pdn.ac.lk>
2024-04-04 21:44:04 +0530

    Practical 01 added

---
+++ Part2/software/jpeg/obj/default/jpeg_encoder.d
@@ -0,0 +1,13 @@
+obj/default/jpeg_encoder.o: jpeg_encoder.c application/jpeg_encoding.h \
+ application/jpegclib/jpeglib.h application/jpegclib/jconfig.h \
+ application/jpegclib/jmorecfg.h application/bmp_extract.h
+
+application/jpeg_encoding.h:
+
+application/jpegclib/jpeglib.h:
+
+application/jpegclib/jconfig.h:
+
+application/jpegclib/jmorecfg.h:
+
+application/bmp_extract.h:
---
+++ Part2/software/jpeg_bsp/obj/drivers/src/alt_quad_seven_seg.d
@@ -0,0 +1,25 @@
+obj/drivers/src/alt_quad_seven_seg.o: drivers/src/alt_quad_seven_seg.c \
+ drivers/inc/alt_quad_seven_seg.h HAL/inc/sys/alt_alarm.h \
+ HAL/inc/sys/alt_llist.h HAL/inc/alt_types.h HAL/inc/priv/alt_alarm.h \
+ system.h linker.h drivers/inc/altera_avalon_pio_regs.h HAL/inc/io.h \
+ HAL/inc/alt_types.h
+
+drivers/inc/alt_quad_seven_seg.h:
+
+HAL/inc/sys/alt_alarm.h:
+
+HAL/inc/sys/alt_llist.h:
+
+HAL/inc/alt_types.h:
+
+HAL/inc/priv/alt_alarm.h:
+
+system.h:
+
+linker.h:
+
+drivers/inc/altera_avalon_pio_regs.h:
+
+HAL/inc/io.h:
+
+HAL/inc/alt_types.h:
